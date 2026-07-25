5f17638c43e1a0b2362297b6a6ee43c4f995eeb4
salma8-3 <salmaaboe1hasaib@gmail.com>
2026-07-25 14:17:29 +0300

    Fix client receiving and add message broadcasting

--- client.py
+++ client.py
@@ -1,4 +1,5 @@
 import socket
+import threading
 
 HOST = "127.0.0.1"
 PORT = 5555
@@ -7,6 +8,22 @@ client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect((HOST, PORT))
 
 print("Connected to the server!")
+
+
+def receive_messages():
+    while True:
+        try:
+            message = client.recv(1024).decode()
+            print("\n" + message)
+        except:
+            print("Disconnected from server")
+            client.close()
+            break
+
+
+thread = threading.Thread(target=receive_messages)
+thread.start()
+
 
 while True:
     message = input("You: ")
--- server.py
+++ server.py
@@ -10,15 +10,36 @@ server.listen()
 
 print(f"Server is running on port {PORT}")
 
+clients = []
+
+
+def broadcast(message, sender):
+    for client in clients:
+        if client != sender:
+            client.send(message)
+
+
 def handle_client(client):
     while True:
-        message = client.recv(1024).decode()
-        print(message)
+        try:
+            message = client.recv(1024)
+            print(message.decode())
+            broadcast(message, client)
+
+        except:
+            clients.remove(client)
+            client.close()
+            break
 
 
 while True:
     client, address = server.accept()
     print(f"New connection from {address}")
 
-    thread = threading.Thread(target=handle_client, args=(client,))
+    clients.append(client)
+
+    thread = threading.Thread(
+        target=handle_client,
+        args=(client,)
+    )
     thread.start()
